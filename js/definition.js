d25bdbc85cf150fb9b201d6c3149e3c7805c1ca1
Rado Andrianjanahary <rado.md92@gmail.com>
2020-04-05 00:43:22 +0200

    added definition support of definition unlink in definition page

--- js/definition.js
+++ js/definition.js
@@ -1,4 +1,4 @@
-import { Get, fetch_language_mapping } from './utils.js'
+import { Get, Put, fetch_language_mapping } from './utils.js'
 
 var app = new Vue({
 	el: '#app',
@@ -9,10 +9,13 @@ var app = new Vue({
 			let id = params.get('defid');
 			let defn = Get(location.origin + "/defw/" + id );
 			let words = defn[0].words[0];
-			console.log('words');
-			console.log(words);					
+			//console.log('words');
+			//console.log(words);					
 			return words
 		}(),
+
+		deleted_words_association: [],
+		new_words_version: [],
 
 		definition: function () {
 			let params = new URLSearchParams(location.search);
@@ -31,25 +34,50 @@ var app = new Vue({
 		addWord: function () {
 		},
 
+		cancelChanges: function () {
+			// Reset changes
+			this.new_words_version = [];
+		},
+
 		saveChanges: function () {
-			this.validate_changes()
+			this.validateChanges()
 			console.log('save Changes called');
-			console.log(this.new_definitions);
+			console.log(this.new_words_version);
 			this.prepareRequest();
 			this.sendRequest();
 		},
 
-		validate_changes: function () {
+		validateChanges: function () {
 			console.log('validate new definitions');
 			console.log('validate edited definitions');
 		},
 
 		prepareRequest: function () {
 			console.log('request prepared')
+			// Definition language
+			this.definition.definition_language = this.definition.language 
+
+			// todo: words
+			for (let j = 0; j < this.new_words_version.length; j++) {
+				for (let k = 0; k < this.new_words_version[j].definitions.length; k++) {
+					this.new_words_version[j].definitions[k].definition_language = this.new_words_version[j].definitions[k].language;
+				}				
+			}		
 		},
 
-		sendRequest: function () {			
-			console.log('request sent')
+		sendRequest: function () {
+			console.log('request sent');
+			// Definition
+			Put('/defn/' + this.definition.id + '/edit', JSON.stringify(this.definition));
+			// Words
+			for (let i = 0; i < this.new_words_version.length; i++) {
+				//console.log('/wrd/' + this.new_words_version[i].id + '/edit');
+				//console.log(JSON.stringify(this.new_words_version[i]));
+				Put('/dict/entry/' + this.new_words_version[i].id + '/edit', JSON.stringify(this.new_words_version[i]));
+			}
+			// Reset changes
+			this.new_words_version = [];
+
 		},
 
 		deleteWord: function (word) {
@@ -58,17 +86,19 @@ var app = new Vue({
 				let index = this.words.indexOf(word);
 				if (index > -1) {
 				  this.words.splice(index, 1);
-				}				
+				}
+				this.deleted_words_association.push(word.id)
 				console.log('deleted in definition list');
-			}			
+				let word_fetched = Get('/wrd/' + word.id);
+				for (let i = 0; i < word_fetched.definitions.length; i++) {
+					if (word_fetched.definitions[i].id === this.definition.id) {
+						word_fetched.definitions.splice(i, 1);
+						this.new_words_version.push(word_fetched);
+						break;
+					}
+					
+				}
+			}
 		},
 	},
-
-	computed: {
-		editCount: function () {			
-			return 0;
-	    }
-	}
-
-
 });
