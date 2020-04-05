a44e299969194fccbd57fa2b389e4b79c5600838
Rado Andrianjanahary <rado.md92@gmail.com>
2020-04-06 00:07:46 +0200

    reorganise files: segregate home code and 3rd party code

---
+++ main.js
@@ -0,0 +1,13 @@
+import { Get, fetch_language_mapping } from './js/utils.js'
+
+var app = new Vue({
+	el: '#app',
+					
+	data: {
+		language_mapping: fetch_language_mapping(),					
+		languages: function () {
+			let words = Get("/dict/list");
+			return words
+		}(), 
+	},
+});
